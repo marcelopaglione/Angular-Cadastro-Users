6336a9566222a5beb081abe7a5b49a5f5f50143c
Marcelo <m.paglionejr@gmail.com>
2018-11-04 17:32:10 -0200

    prepare to data driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataFormComponent } from './data-form/data-form.component';
 import { HttpClient } from 'selenium-webdriver/http';
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { TemplateFormModule } from './template-form/template-form.module';
+import { DataFormModule } from './data-form/data-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataFormComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,    
     HttpModule,
     TemplateFormModule,
+    ReactiveFormsModule,
+    DataFormModule,
     AppRoutingModule
   ],
   providers: [],
