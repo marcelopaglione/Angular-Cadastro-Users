8e8d589f29401c5eca0be5e55895946b85116db3
Marcelo <m.paglionejr@gmail.com>
2018-11-04 13:14:52 -0200

    refactor do new template module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { HttpClient } from 'selenium-webdriver/http';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     DataFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,    
+    HttpModule,
+    TemplateFormModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario: any = {
+    nome: 'Marcelo Ortiz Paglione Junior',
+    email: 'Marcelo.Paglione@gmail.com'
+  }
+
   constructor() { }
+
+  onSubmit(form){
+    console.log(form);
+    console.log(form.value);
+    console.log(this.usuario);
+  }
 
   ngOnInit() {
   }
